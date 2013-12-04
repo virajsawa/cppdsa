ca9208eac2b55a00a50d80ea4db7733b298005d6
viraj <virajsawa@gmail.com>
2013-12-05 01:36:55 +0530

    different data types array and mode number

--- template_array.cpp
+++ template_array.cpp
@@ -4,27 +4,28 @@
 using namespace std; 
 
 template <class T>
-int mode(int[], int, T);
+int mode(T[], int, T);
 
 int main(){
 
 int bar=3;
-float foo=3.2;
+float foo=4.2;
 int len = 10;
 //cout<<"Enter the number : ";
 //cin>>number;
 int inarr[] = {1,2,3,3,4,5,6,4,5,6};
 cout<<mode(inarr, len, bar)<<"\n";
-cout<<mode(inarr, len, foo)<<"\n";
+float flarr[] = {4.2, 1.0, 2.3, 4.2, 5.6, 4.5, 4.2, 9.0, 7.1, 4.1};
+cout<<mode(flarr, len, foo)<<"\n";
 
 return 0;
 }
 
 template <class T>
-int mode(int array[], int len, T num){
+int mode(T array[], int len, T num){
 int counter=0;
 for(int i=0;i<len;i++){
-if(array[i]==((int)num)){counter++;}
+if(array[i]==num){counter++;}
 }
 return counter;
 } 
