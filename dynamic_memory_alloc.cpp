3edad87b00cf77f5d5dcfba4dd2281b282df5a55
viraj <virajsawa@gmail.com>
2013-12-07 21:59:21 +0530

    One dimentional array dynamically defined

--- dynamic_memory_alloc.cpp
+++ dynamic_memory_alloc.cpp
@@ -5,12 +5,21 @@ using namespace std;
 
 int main(){
 
-int *x;
+int n;
 
-x = new int (10);
+cout<<"Enter the size of array :";
+cin>>n;
 
-cout<<x<<"\n";
-cout<<*x<<"\n";
+int *x = new int [n];
+int i;
+for(i=0;i<n;i++){
+x[i]=i+1;
+}
+
+cout<<"\nEnter the number of element do you wish to see : ";
+cin>>i;
+cout<<"Number is : "<<x[i-1]<<"\n";
+cout<<"And address is : "<<&x[i]<<"\n";
 
 return 0;
 }
